5d45f1330ffe145d1e98d6265d4bea3178034400
dbian <zqqbhd@icloud.com>
2023-12-09 15:28:40 +0800

    enhance tokenizer

--- pyscheme/tokenizer.py
+++ pyscheme/tokenizer.py
@@ -1,16 +1,36 @@
 import re
+from typing import List, Tuple
+Token = Tuple[str, str | int | float]
 
 
-def tokenize(scheme_code):
+class TokenType:
+    COMMENT_BLOCK = "COMMENT_BLOCK"
+    COMMENT_LINE = "COMMENT_LINE"
+    STRING = "STRING"
+    NUMBER = "NUMBER"
+    WORD = "WORD"
+    PAREN_L = "PAREN_L"
+    PAREN_R = "PAREN_R"
+    QUOTE = "QUOTE"
+    QQUOTE = "QQUOTE"
+    COMMA = "COMMA"
+    COMMA_AT = "COMMA_AT"
+
+
+def tokenize(scheme_code: str) -> List[Token]:
     # 定义正则表达式模式
     patterns = [
-        # (r'#\|.*?\|#', 'COMMENT_BLOCK'),  # 匹配 /* 注释 */
-        (r';.*\n?', 'COMMENT_LINE'),  # 匹配行注释
-        (r'"(?:\\.|[^"])*"', 'STRING'),  # 匹配字符串
-        (r'[-+]?\d+(?:\.\d+)?', 'NUMBER'),  # 匹配数字
-        (r'[^\s\(\)"\';]+', 'WORD'),  # 匹配单词
-        (r'\(|\)', 'PAREN'),  # 匹配括号
-        (r"'", 'QUOTE'),  # 匹配引号
+        # (r'#\|.*?\|#', TokenType.COMMENT_BLOCK),  # 匹配 /* 注释 */
+        (r';.*\n?', TokenType.COMMENT_LINE),  # 匹配行注释
+        (r'"(?:\\.|[^"])*"', TokenType.STRING),  # 匹配字符串
+        (r'[-+]?\d+(?:\.\d+)?', TokenType.NUMBER),  # 匹配数字
+        (r'\(', TokenType.PAREN_L),  # 匹配括号
+        (r'\)', TokenType.PAREN_R),  # 匹配括号
+        (r"'", TokenType.QUOTE),  # 匹配引号
+        (r"`", TokenType.QQUOTE),
+        (r",@", TokenType.COMMA_AT),
+        (r",", TokenType.COMMA),
+        (r'[^\s\(\)"\';]+', TokenType.WORD),  # 匹配单词
     ]
 
     token_regex = '|'.join('(?P<{}>{})'.format(name, pattern)
@@ -19,6 +39,16 @@ def tokenize(scheme_code):
     for match in re.finditer(token_regex, scheme_code):
         kind = match.lastgroup
         value = match.group()
+        match kind:
+            case TokenType.COMMENT_LINE:
+                continue
+            case TokenType.STRING:
+                value = value[1:-1]
+            case TokenType.NUMBER:
+                try:
+                    value = int(value)
+                except ValueError:
+                    value = float(value)
         tokens.append((kind, value))
 
     return tokens
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -5,21 +5,36 @@ import pyscheme.tokenizer as tokenizer
 class TestT(unittest.TestCase):
     def test_comment(self):
         scheme_code = '''
-        ; This is a comment
-        #| This is a 
-        ; block comment |#
+        ; ( This )is a comment
         (define x 10)
         (define y 20.5)
+        (define str "Hello#|,() world!|# ; fake comment")
         (define 'a 30)
-        (define str "Hello#|, world!|# ; fake comment")
-        (display (+ x y))
+        `(,display ,@(list a b))
     '''
         tk = tokenizer.tokenize(scheme_code)
-        self.assertEqual(tk[0], ("COMMENT_LINE", "; This is a comment\n"))
+        self.assertEqual(tk[0], (tokenizer.TokenType.PAREN_L, "("))
+        self.assertEqual(tk[8], (tokenizer.TokenType.NUMBER, 20.5))
+        self.assertEqual(
+            tk[13], (tokenizer.TokenType.STRING, "Hello#|,() world!|# ; fake comment"))
+        self.assertEqual(tk[14], (tokenizer.TokenType.PAREN_R, ")"))
+        self.assertEqual(tk[17], (tokenizer.TokenType.QUOTE, "'"))
+        self.assertEqual(tk[21], (tokenizer.TokenType.QQUOTE, "`"))
+        self.assertEqual(tk[23], (tokenizer.TokenType.COMMA, ","))
+        self.assertEqual(tk[25], (tokenizer.TokenType.COMMA_AT, ",@"))
 
         scheme_code = '''; This is a comment'''
         tk = tokenizer.tokenize(scheme_code)
-        self.assertEqual(tk[0], ("COMMENT_LINE", "; This is a comment"))
+        self.assertEqual(len(tk), 0)
+
+    def test_quote(self):
+        scheme_code = '''
+            (define a 30)
+        '''
+        tk = tokenizer.tokenize(scheme_code)
+        self.assertEqual(tk[2], (tokenizer.TokenType.WORD, "a"))
+        self.assertEqual(tk[3], (tokenizer.TokenType.NUMBER, 30))
+        self.assertEqual(tk[4], (tokenizer.TokenType.PAREN_R, ")"))
 
 
 if __name__ == '__main__':
