0007647f49dfbf5c8858f3e71989ab19890b7ec3
dbian <zqqbhd@icloud.com>
2023-12-12 15:29:50 +0800

    add rec

--- tests/test_macro.py
+++ tests/test_macro.py
@@ -14,6 +14,11 @@ class TestParser(unittest.TestCase):
         f = pyscheme.run_file("tests/macro2.scm", env)
         self.assertEqual(f, 1)
 
+    def test_macro3(self):
+        env = pyscheme.new_env()
+        f = pyscheme.run_file("tests/macro_rec.scm", env)
+        self.assertEqual(f, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
